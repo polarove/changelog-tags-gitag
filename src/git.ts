23ee9a115b373eec518188b61439e6d3dfeab4c1
polarove <liuqi6602@163.com>
2024-04-24 10:37:36 +0800

    151

--- src/git.ts
+++ src/git.ts
@@ -1,3 +1,17 @@
+import { execute } from './command'
 import { TagFrom, TagTo } from './types'
+
+export const getGitTags = async (from?: TagFrom, to?: TagTo) => {
+  if (from && to) return { previous: from, latest: to }
+  const tags = await execute('git', [
+    '--no-pager',
+    'tag',
+    '-l',
+    '--sort=-creatordate'
+  ])
+  const previous = tags.split('\n')[1]
+  const latest = tags.split('\n')[0]
+  return { previous, latest }
+}
 
 export const getCommitsBetweenTags = (tagFrom: TagFrom, tagTo: TagTo) => {}
